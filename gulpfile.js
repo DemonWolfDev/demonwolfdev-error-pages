733993fe47b1a7873b7385777a343d2ae62de5ab
Nathan Henniges <demonwolf@demonwolfdev.com>
2017-09-15 10:23:03 -0500

    Added stuff.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
 var bs = require('browser-sync').create();
 
+gulp.task('default', ['watch']);
+
 // Runs the browser sync server
-gulp.task('browser-sync', ['sass'], function() {
+gulp.task('browser-sync', function() {
     bs.init({
         server: {
             baseDir: "./",
@@ -11,25 +12,7 @@ gulp.task('browser-sync', ['sass'], function() {
     });  
 });
 
-// Compiles the sass and logs error
-gulp.task('sass', function () {
-  gulp.src('./sass/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sass({outputStyle: 'compact'}))
-    .pipe(gulp.dest('./css/'));
-});
-// Here to rerun the sass when a error happens
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/*.scss', ['sass']);
-});
-
 gulp.task('watch', ['browser-sync'], function () {
-    // Sass
-    gulp.watch('./sass/*.scss', ['sass']);
-    // CSS
-    gulp.watch("./css/style.css").on('change', bs.reload);
-    // JS
-    gulp.watch("./js//script.js").on('change', bs.reload);
     // Error codes
     gulp.watch("./401/index.html").on('change', bs.reload);
     gulp.watch("./403/index.html").on('change', bs.reload);
